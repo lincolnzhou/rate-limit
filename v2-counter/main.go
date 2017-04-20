a07c80a742aeb24791dd2700dadda382e4e21da9
lincolnzhou <875199116@qq.com>
2017-04-20 09:17:40 +0800

    v2-cpunter

--- v2-counter/main.go
+++ v2-counter/main.go
@@ -4,21 +4,24 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+	"sync/atomic"
 )
 
 var (
 	GRequest = make(chan int, 0)
+	Count int64 = 0
 )
 
 func main() {
-	println("【rate-limit】v1-timer")
+	println("【rate-limit】v2-counter")
 
 	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		GRequest <- 1
 		w.Write([]byte("ok"))
 	})
 
-	go v1_timer()
+	go clearCount()
+	go handleRequest()
 
 	addr := "0.0.0.0:7000"
 	s := &http.Server{
@@ -29,11 +32,23 @@ func main() {
 	fmt.Println(s.ListenAndServe())
 }
 
-func v1_timer() {
+func clearCount() {
 	ticker := time.Tick(time.Millisecond * 200)
 
-	for range ticker {
-		req := <-GRequest
-		fmt.Println(time.Now(), "Handling request", req)
+	go func() {
+		for range ticker {
+			atomic.StoreInt64(&Count, 0)
+		}
+	}()
+}
+
+func handleRequest() {
+	for req := range GRequest {
+		if Count < 5 {
+			atomic.AddInt64(&Count, 1)
+			fmt.Println(time.Now(), "Handling request", req)
+		} else {
+			fmt.Println(time.Now(), "Denying request", req)
+		}
 	}
 }
